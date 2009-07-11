9a31bd693b9919ed7de02e5aea8d7bc9c27c98dc
Run Paint Run Run <runrun@runpaint.org>
2009-07-11 04:14:15 +0100

    Signature: Initial implementation of class

--- lib/readruby.rb
+++ lib/readruby.rb
@@ -1,4 +1,5 @@
 module ReadRuby
 end
 require File.dirname(__FILE__) + '/readruby/example'
+require File.dirname(__FILE__) + '/readruby/signature'
 
---
+++ lib/readruby/signature.rb
@@ -0,0 +1,43 @@
+module ReadRuby
+  class Signature
+    # Matches a class name, e.g. String or NilClass
+    CLASS_PAT = '([A-Z]\w+)(?:\W+|$)'
+    # Matches the method signature. We are intentionally vague about the
+    # return type declaration because it's more readable to use #scan on the
+    # captured portion than delegate both tasks to the regex
+    SIGNATURE_REX = /^\s{4}.+ => \s*(#{CLASS_PAT}.*)$/o
+
+    attr_accessor :object, :method, :description
+
+    def initialize(object, method, description)
+      @object = object
+      raise ArgumentError unless @object.is_a?(Class)
+      @method = method
+      raise ArgumentError unless @method.is_a?(Symbol)
+      @description = description
+      raise ArgumentError unless @description.is_a?(String)
+      raise NoMethodError, "#{object}##{method}" unless valid_method?
+    end
+
+    def returns
+      match = self.description.split(/\n/)[0].match(SIGNATURE_REX) or return []
+      match[1].scan(/#{CLASS_PAT}/).map do |type|
+        type = type.first
+        begin
+          Object.const_get(type.to_sym)
+        rescue NameError
+          raise NameError, "Invalid return type: #{type.to_sym}"
+        end
+      end
+    end
+
+    private
+    def valid_method?
+      self.object.method_defined?(self.method) ||
+      # We use this ugly construct so as to be compatiable with both 1.8,
+      # where .singleton_methods returned Strings, and 1.9, where it returns
+      # Symbols 
+      self.object.singleton_methods.any? {|m| m.to_sym == self.method }
+    end
+  end
+end
