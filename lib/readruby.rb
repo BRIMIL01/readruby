03da22bf155c90b4c338577060854c5e6523d109
Run Paint Run Run <runrun@runpaint.org>
2009-07-12 00:22:57 +0100

    Description: Initially implement Description class

--- lib/readruby.rb
+++ lib/readruby.rb
@@ -3,4 +3,5 @@ end
 require File.dirname(__FILE__) + '/readruby/example'
 require File.dirname(__FILE__) + '/readruby/signature'
 require File.dirname(__FILE__) + '/readruby/invocation'
+require File.dirname(__FILE__) + '/readruby/description'
 
---
+++ lib/readruby/description.rb
@@ -0,0 +1,28 @@
+module ReadRuby
+  class Description
+
+    attr_accessor :text
+
+    def initialize(description)
+      @text = description
+      raise ArgumentError unless @text.is_a?(String)
+    end
+
+    def html
+      self.class.markdown.new(self.text).to_html
+    end
+
+    def self.markdown
+      return @@markdown if defined?(@@markdown)
+      @@markdown = nil
+      require 'rubygems'
+      %w{RDiscount Maruku BlueCloth}.each do |lib| 
+        begin
+          require lib.downcase
+          return @@markdown = Object.const_get(lib)
+        rescue LoadError
+        end
+      end or raise LoadError, "No Markdown converter found"
+    end
+  end
+end
