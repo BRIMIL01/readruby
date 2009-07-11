3b169db7f597f7354326160203ef579c749f8e09
Run Paint Run Run <runrun@runpaint.org>
2009-07-11 23:54:54 +0100

    Signature: Refactored to only deal with signature and return value

--- lib/readruby/signature.rb
+++ lib/readruby/signature.rb
@@ -1,26 +1,35 @@
 module ReadRuby
   class Signature
     # Matches a class name, e.g. String or NilClass
-    CLASS_PAT = '([A-Z]\w+)(?:\W+|$)'
+    CLASS_PAT = '([A-Z]\w+)'
+    VAR_PAT = '([a-z0-9_]\w+)'
     # Matches the method signature. We are intentionally vague about the
     # return type declaration because it's more readable to use #scan on the
     # captured portion than delegate both tasks to the regex
-    SIGNATURE_REX = /^\s{4}.+ => \s*(#{CLASS_PAT}.*)$/o
+    SIGNATURE_REX = /^\s{4}\s*\(([^\)]*)\)\s*=> \s*(#{CLASS_PAT}.*)$/o
 
-    attr_accessor :object, :method, :description
+    attr_accessor :description
 
-    def initialize(object, method, description)
-      @object = object
-      raise ArgumentError unless @object.is_a?(Class)
-      @method = method
-      raise ArgumentError unless @method.is_a?(Symbol)
+    def initialize(description)
       @description = description
       raise ArgumentError unless @description.is_a?(String)
-      raise NoMethodError, "#{object}##{method}" unless valid_method?
+    end
+
+    def signature
+      match = self.description.split(/\n/)[0][SIGNATURE_REX, 1] or return [[]]
+      sig = match.scan(/#{CLASS_PAT}( #{VAR_PAT})?/o).map do |type, variable|
+        begin
+          type = Object.const_get(type.to_sym)
+        rescue NameError
+          raise NameError, "Invalid signature type: #{type.to_sym}"
+        end
+        variable.nil? ? [type] : [type, variable.strip.to_sym]
+      end
+      sig.empty? ? [[]] : sig
     end
 
     def returns
-      match = self.description.split(/\n/)[0][SIGNATURE_REX, 1] or return []
+      match = self.description.split(/\n/)[0][SIGNATURE_REX, 2] or return []
       match.scan(/#{CLASS_PAT}/o).map do |type|
         type = type.first
         begin
@@ -31,13 +40,5 @@ module ReadRuby
       end
     end
 
-    private
-    def valid_method?
-      self.object.method_defined?(self.method) ||
-      # We use this ugly construct so as to be compatiable with both 1.8,
-      # where .singleton_methods returned Strings, and 1.9, where it returns
-      # Symbols 
-      self.object.singleton_methods.any? {|m| m.to_sym == self.method }
-    end
   end
 end
--- spec/readruby/signature_spec.rb
+++ spec/readruby/signature_spec.rb
@@ -1,142 +1,170 @@
 describe ReadRuby::Signature, ".new" do
-  it "requires a Class as the first argument" do
+  it "requires a String argument" do
     lambda do
-      ReadRuby::Signature.new(String, :[], 'text') 
+      ReadRuby::Signature.new('text') 
     end.should_not raise_error
 
     lambda do
-      ReadRuby::Signature.new('string', :[], 'text') 
-    end.should raise_error(ArgumentError)
-  end
-
-  it "requires a Symbol as the second argument" do
-    lambda do
-      ReadRuby::Signature.new(Array, :size, 'text') 
-    end.should_not raise_error
-
-    lambda do
-      ReadRuby::Signature.new(File, [], 'text') 
-    end.should raise_error(ArgumentError)
-  end
-
-  it "raises a NoMethodError if the given object has the given method" do
-    lambda do
-      ReadRuby::Signature.new(File, :glark, 'text') 
-    end.should raise_error(NoMethodError)
-  end
-
-  it "doesn't raise a NoMethodError if the given object has the given instance method" do
-    lambda do
-      ReadRuby::Signature.new(File, :path, 'text') 
-    end.should_not raise_error(NoMethodError)
-  end
-
-  it "doesn't raise a NoMethodError if the given object has the given class method" do
-    lambda do
-      ReadRuby::Signature.new(File, :open, 'text') 
-    end.should_not raise_error(NoMethodError)
-  end
-
-  it "requires a String for the final argument" do
-    lambda do
-      ReadRuby::Signature.new(File, :size, 'text') 
-    end.should_not raise_error
-
-    lambda do
-      ReadRuby::Signature.new(File, :open, :size) 
+      ReadRuby::Signature.new(:size) 
     end.should raise_error(ArgumentError)
   end
 
   it "returns an instance of ReadRuby::Signature" do
-    ReadRuby::Signature.new(Array, :size, 'foo').should 
+    ReadRuby::Signature.new('foo').should 
       be_an_instance_of(ReadRuby::Signature)
-  end
-end
-
-describe ReadRuby::Signature, "#object" do
-  it "returns a Class" do
-    ReadRuby::Signature.new(Array, :first, 'text').object.should 
-      be_an_instance_of(Class)
-  end
-
-  it "returns the first argument passed to the constructor" do
-    ReadRuby::Signature.new(Array, :first, 'text').object.should == Array
-  end
-end
-
-describe ReadRuby::Signature, "#method" do
-  it "returns a Symbol" do
-    ReadRuby::Signature.new(Array, :first, 'text').method.should 
-      be_an_instance_of(Symbol)
-  end
-
-  it "returns the second argument passed to the constructor" do
-    ReadRuby::Signature.new(Array, :first, 'text').method.should == :first
   end
 end
 
 describe ReadRuby::Signature, "#returns" do
   it "returns an Array" do
-    ReadRuby::Signature.new(Array, :first, 'text').returns.should 
+    ReadRuby::Signature.new('text').returns.should 
       be_an_instance_of(Array)
   end
 
   it "returns an Array of Class objects if return types were specified" do
-    ReadRuby::Signature.new(Array, :first, '    () => Object').returns.
+    ReadRuby::Signature.new('    () => Object').returns.
       all? {|o| o.should be_a_kind_of(Class) }
   end
 
   it "returns the return value specified in the signature" do
-    ReadRuby::Signature.new(Array, :first, '    () => Object').
+    ReadRuby::Signature.new('    () => Object').
       returns.should == [Object]
   end
 
   it "understands signatures with paramater types" do
-    ReadRuby::Signature.new(String, :slice, '    (Fixnum offset) => String').
+    ReadRuby::Signature.new('    (Fixnum offset) => String').
       returns.should == [String]
   end
 
   it "ignores leading whitespace" do
-    ReadRuby::Signature.new(Array, :first, '    () =>    Object').
+    ReadRuby::Signature.new('    () =>    Object').
       returns.should == [Object]
   end
 
   it "ignores trailing whitespace" do
-    ReadRuby::Signature.new(Array, :first, '    () => Object   ').
+    ReadRuby::Signature.new('    () => Object   ').
       returns.should == [Object]
   end
 
   it "recognises multiple return types separated by ' or '" do
-    ReadRuby::Signature.new(Array, :first, '    () => Object or NilClass').
+    ReadRuby::Signature.new('    () => Object or NilClass').
       returns.should == [Object, NilClass]
   end
 
   it "recognises multiple return types separated by ', '" do
-    ReadRuby::Signature.new(Array, :first, '    () => Object, NilClass').
+    ReadRuby::Signature.new('    () => Object, NilClass').
       returns.should == [Object, NilClass]
   end
 
   it "recognises multiple return types separated by ' '" do
-    ReadRuby::Signature.new(Array, :first, '    () => Object  NilClass').
+    ReadRuby::Signature.new('    () => Object  NilClass').
       returns.should == [Object, NilClass]
   end
 
   it "raises a NameError if the return type is not a valid constant" do
     lambda do
-      ReadRuby::Signature.new(Array, :first, '    () => Objectified').returns
+      ReadRuby::Signature.new('    () => Objectified').returns
     end.should raise_error(NameError)
   end
 
   it "raises a NameError if one return type is not a valid constant, but others are" do
     lambda do
-      ReadRuby::Signature.new(
-        Array, :first, '    () => Hash Objectified'
-      ).returns
+      ReadRuby::Signature.new('    () => Hash Objectified').returns
     end.should raise_error(NameError)
   end
 
   it "returns an empty Array when no signature was found" do
-    ReadRuby::Signature.new(Array, :first, 'text').
+    ReadRuby::Signature.new('text').
       returns.should == []
+  end
+end
+
+describe ReadRuby::Signature, "#signature" do
+  it "returns an Array" do
+    ReadRuby::Signature.new('text').signature.should 
+      be_an_instance_of(Array)
+  end
+
+  it "returns each signature element as a sub-Array" do
+    ReadRuby::Signature.new('    (Fixnum) => Object').signature.
+      all? {|o| o.should be_a_kind_of(Array) }
+  end
+
+  it "sets first element of each sub-Array to a Class if types were specified" do
+    ReadRuby::Signature.new('    (Fixnum) => Object').signature.
+      all? {|a| a.first.should be_a_kind_of(Class) }
+  end
+
+  it "sets last element of each sub-Array to a Symbol if paramater names were specified" do
+    ReadRuby::Signature.new('    (Fixnum offset) => Object').signature.
+      all? {|a| a.last.should be_a_kind_of(Symbol) }
+  end
+
+  it "returns the single type specified in the signature" do
+    ReadRuby::Signature.new('    (Fixnum) => Object').
+      signature.should == [[Fixnum]]
+  end
+
+  it "returns the types specified in the signature" do
+    ReadRuby::Signature.new('    (Fixnum, String) => Object').
+      signature.should == [[Fixnum], [String]]
+  end
+
+  it "returns the single type-paramater pair specified in the signature" do
+    ReadRuby::Signature.new('    (Fixnum index) => Object').
+      signature.should == [[Fixnum, :index]]
+  end
+
+  it "returns the type-paramater pairs specified in the signature" do
+    ReadRuby::Signature.new('    (Fixnum index, String name) => Object').
+      signature.should == [[Fixnum, :index], [String, :name]]
+  end
+
+  it "returns [[]] when no signature is given" do
+    ReadRuby::Signature.new('    () =>    Object').
+      signature.should == [[]]
+  end
+
+  it "ignores leading whitespace" do
+    ReadRuby::Signature.new('           (Fixnum) => Object').
+      signature.should == [[Fixnum]]
+  end
+
+  it "ignores trailing whitespace" do
+    ReadRuby::Signature.new('    (Fixnum)                 => Object').
+      signature.should == [[Fixnum]]
+  end
+
+  it "allows types to be separated by ', '" do
+    ReadRuby::Signature.new('    (Fixnum, String) => Object or NilClass').
+      signature.should == [[Fixnum], [String]]
+  end
+
+  it "allows type-paramater pairs to be separated by ', '" do
+    ReadRuby::Signature.new('    (Fixnum index, String) => Object or NilClass').
+      signature.should == [[Fixnum, :index], [String]]
+  end
+
+  it "recognises types separated by ' '" do
+    ReadRuby::Signature.new('    (Fixnum Float Integer) => Object  NilClass').
+      signature.should == [[Fixnum], [Float], [Integer]]
+  end
+
+  it "recognises type-paramater pairs separated by ' '" do
+    ReadRuby::Signature.new('    (Float glark Integer bar) => Object or NilClass').
+      signature.should == [[Float, :glark], [Integer, :bar]]
+  end
+
+  it "raises a NameError if a type is not a valid constant" do
+    lambda do
+      ReadRuby::Signature.new('    (Floating) => Object').signature
+    end.should raise_error(NameError)
+  end
+
+  it "raises a NameError if one type is not a valid constant, but others are" do
+    lambda do
+      ReadRuby::Signature.new(
+        '    (Integer, Filed file) => Hash Object').signature
+    end.should raise_error(NameError)
   end
 end
